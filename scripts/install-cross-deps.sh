a1dec6a014371baad48acc269ea997fb725f748d
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-01-10 05:18:51 +0100

    Update install-cross-deps.sh

--- scripts/install-cross-deps.sh
+++ scripts/install-cross-deps.sh
@@ -8,8 +8,12 @@ TARGET="$1"
 
 case "$TARGET" in
     aarch64-unknown-linux-gnu)
+        sudo dpkg --add-architecture arm64
+        sudo sed -i 's/^deb /deb [arch=amd64] /' /etc/apt/sources.list
+        echo "deb [arch=arm64] http://ports.ubuntu.com/ $(lsb_release -cs) main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
+        echo "deb [arch=arm64] http://ports.ubuntu.com/ $(lsb_release -cs)-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
         sudo apt-get update
-        sudo apt-get install -y gcc-aarch64-linux-gnu
+        sudo apt-get install -y gcc-aarch64-linux-gnu libssl-dev:arm64 pkg-config
         ;;
     *)
         # No extra dependencies needed
